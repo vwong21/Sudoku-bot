a9b8b5f92c98bc28754d886a43e64e9c732c848e
Vincent Wong <vincentwong5609@gmail.com>
2025-03-07 16:53:35 -0800

    merged make and solve function

--- sudoku.py
+++ sudoku.py
@@ -14,7 +14,7 @@ def is_valid(board, row, col, num):
     return True
 
 
-def make_board(board, row, col):
+def solve_board(board, row, col):
     if row == 8 and col == 9:
         return True
     if col == 9:
@@ -22,14 +22,14 @@ def make_board(board, row, col):
         col = 0
 
     if board[row][col] != 0:
-        return make_board(board, row, col + 1)
+        return solve_board(board, row, col + 1)
 
     nums = list(range(1, 10))
     random.shuffle(nums)
     for num in nums:
         if is_valid(board, row, col, num):
             board[row][col] = num
-            if make_board(board, row, col + 1):
+            if solve_board(board, row, col + 1):
                 return True
             board[row][col] = 0
     return False
@@ -37,7 +37,7 @@ def make_board(board, row, col):
 
 def create_sudoku():
     board = [[0] * 9 for _ in range(9)]
-    make_board(board, 0, 0)
+    solve_board(board, 0, 0)
     return board
 
 
@@ -52,25 +52,6 @@ def remove_nums(board, num_holes=40):
     return empty_board
 
 
-def solve_board(puzzle, row, col):
-    if row == 8 and col == 9:
-        return True
-    if col == 9:
-        row += 1
-        col = 0
-
-    if puzzle[row][col] != 0:
-        return solve_board(puzzle, row, col + 1)
-
-    for num in range(1, 10):
-        if is_valid(puzzle, row, col, num):
-            puzzle[row][col] = num
-            if solve_board(puzzle, row, col + 1):
-                return True
-            puzzle[row][col] = 0
-    return False
-
-
 if __name__ == "__main__":
     reveal = create_sudoku()
     puzzle = remove_nums(reveal, num_holes=42)
@@ -78,6 +59,7 @@ if __name__ == "__main__":
     for row in puzzle:
         print(row)
 
+    print()
     solved = solve_board(puzzle, 0, 0)
     print("Solved: ")
 
